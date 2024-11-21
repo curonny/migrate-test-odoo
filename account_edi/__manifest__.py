08bf57f9b74d662f8fcc68ea2d2bd8285401526d
Ronny <rmontano1992@gmail.com>
2024-11-21 13:56:34 -0600

    [MIG] account_edi: Migration to 15.0

--- account_edi/__manifest__.py
+++ account_edi/__manifest__.py
@@ -1,29 +1,22 @@
-# -*- coding: utf-8 -*-
 {
-    'name' : 'Import/Export Invoices From XML/PDF',
-    'description':"""
-Electronic Data Interchange
-=======================================
-EDI is the electronic interchange of business information using a standardized format.
-
-This is the base module for import and export of invoices in various EDI formats, and the
-the transmission of said documents to various parties involved in the exchange (other company,
-governements, etc.)
-    """,
-    'version' : '1.0',
-    'category': 'Accounting/Accounting',
-    'depends' : ['account'],
-    'data': [
-        'security/ir.model.access.csv',
-        'wizard/account_invoice_send_views.xml',
-        'views/account_edi_document_views.xml',
-        'views/account_move_views.xml',
-        'views/account_payment_views.xml',
-        'views/account_journal_views.xml',
-        'data/cron.xml'
+    "name": "Import/Export Invoices From XML/PDF",
+    "description": "\nElectronic Data Interchange\n=======================================\nEDI is the electronic interchange of business information using a standardized format.\n\nThis is the base module for import and export of invoices in various EDI formats, and the\nthe transmission of said documents to various parties involved in the exchange (other company,\ngovernements, etc.)\n    ",
+    "version": "15.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "depends": [
+        "account"
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': True,
-    'license': 'LGPL-3',
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/account_invoice_send_views.xml",
+        "views/account_edi_document_views.xml",
+        "views/account_move_views.xml",
+        "views/account_payment_views.xml",
+        "views/account_journal_views.xml",
+        "data/cron.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+    "license": "LGPL-3"
 }
