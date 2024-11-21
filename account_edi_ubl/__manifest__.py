08e02960b2b44548ed1fd3f0c7a595632f39a9b6
Ronny <rmontano1992@gmail.com>
2024-11-21 13:56:32 -0600

    [MIG] account_edi_ubl: Migration to 15.0

--- account_edi_ubl/__manifest__.py
+++ account_edi_ubl/__manifest__.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
 {
-    'name' : 'Import/Export invoices with generic UBL',
-    'version' : '1.0',
-    'category': 'Accounting/Accounting',
-    'depends' : ['account_edi'],
-    'data': [
-        'data/ubl_templates.xml',
+    "name": "Import/Export invoices with generic UBL",
+    "version": "15.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "depends": [
+        "account_edi"
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': True,
-    'license': 'LGPL-3',
+    "data": [
+        "data/ubl_templates.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+    "license": "LGPL-3"
 }
