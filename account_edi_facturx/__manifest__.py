f560010af44de170db628f6e658b9891d894c25b
Ronny <rmontano1992@gmail.com>
2024-11-21 13:56:32 -0600

    [MIG] account_edi_facturx: Migration to 15.0

--- account_edi_facturx/__manifest__.py
+++ account_edi_facturx/__manifest__.py
@@ -1,16 +1,17 @@
-# -*- coding: utf-8 -*-
 {
-    'name' : 'Import/Export invoices with Factur-X',
-    'version' : '1.0',
-    'category': 'Accounting/Accounting',
-    'depends' : ['account_edi'],
-    'data': [
-        'data/account_edi_data.xml',
-        'data/facturx_templates.xml',
+    "name": "Import/Export invoices with Factur-X",
+    "version": "15.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "depends": [
+        "account_edi"
     ],
-    'installable': True,
-    'post_init_hook': '_post_init_hook',
-    'application': False,
-    'auto_install': True,
-    'license': 'LGPL-3',
+    "data": [
+        "data/account_edi_data.xml",
+        "data/facturx_templates.xml"
+    ],
+    "installable": True,
+    "post_init_hook": "_post_init_hook",
+    "application": False,
+    "auto_install": True,
+    "license": "LGPL-3"
 }
