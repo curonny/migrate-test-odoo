fd410657f144506b18c6edfd6d19d0d47c32addc
Ronny <rmontano1992@gmail.com>
2024-11-21 13:57:45 -0600

    [MIG] account_edi_extended: Migration to 15.0

--- account_edi_extended/__manifest__.py
+++ account_edi_extended/__manifest__.py
@@ -1,20 +1,19 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Additionnal features for account_edi",
-    "description": """
-        This module add features to account_edi to support new Edi formats.
-    """,
-    "version": "1.0",
+    "description": "\n        This module add features to account_edi to support new Edi formats.\n    ",
+    "version": "15.0.1.0.0",
     "category": "Accounting/Accounting",
-    "depends": ["account_edi"],
+    "depends": [
+        "account_edi"
+    ],
     "data": [
         "views/account_edi_document_views.xml",
         "views/account_move_views.xml",
-        "views/account_payment_views.xml",
+        "views/account_payment_views.xml"
     ],
     "installable": True,
     "application": False,
     "auto_install": False,
     "post_init_hook": "account_edi_block_level",
-    "license": "LGPL-3",
+    "license": "LGPL-3"
 }
