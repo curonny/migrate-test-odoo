535452c5242adbb65e2d869db2f916fc6ba3eb7a
Ronny <rmontano1992@gmail.com>
2024-11-21 13:57:45 -0600

    [MIG] account_check_printing: Migration to 15.0

--- account_check_printing/__manifest__.py
+++ account_check_printing/__manifest__.py
@@ -1,17 +1,12 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
     "name": "Check Printing Base",
-    "version": "1.0",
+    "version": "15.0.1.0.0",
     "category": "Accounting/Accounting",
     "summary": "Check printing commons",
-    "description": """
-This module offers the basic functionalities to make payments by printing checks.
-It must be used as a dependency for modules that provide country-specific check templates.
-The check settings are located in the accounting journals configuration page.
-    """,
-    "depends": ["account"],
+    "description": "\nThis module offers the basic functionalities to make payments by printing checks.\nIt must be used as a dependency for modules that provide country-specific check templates.\nThe check settings are located in the accounting journals configuration page.\n    ",
+    "depends": [
+        "account"
+    ],
     "data": [
         "security/ir.model.access.csv",
         "data/account_check_printing_data.xml",
@@ -20,9 +15,9 @@ The check settings are located in the accounting journals configuration page.
         "views/account_payment_views.xml",
         "views/res_config_settings_views.xml",
         "views/res_partner_views.xml",
-        "wizard/print_prenumbered_checks_views.xml",
+        "wizard/print_prenumbered_checks_views.xml"
     ],
     "installable": True,
     "auto_install": False,
-    "license": "LGPL-3",
+    "license": "LGPL-3"
 }
