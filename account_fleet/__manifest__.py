f8b7a3dee0ddbac30104d076959b429e21532040
Ronny <rmontano1992@gmail.com>
2024-11-21 13:56:32 -0600

    [MIG] account_fleet: Migration to 15.0

--- account_fleet/__manifest__.py
+++ account_fleet/__manifest__.py
@@ -1,19 +1,20 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Accounting/Fleet bridge',
-    'category': 'Accounting/Accounting',
-    'summary': 'Manage accounting with fleets',
-    'description': "",
-    'version': '1.0',
-    'depends': ['fleet', 'account'],
-    'data': [
-        'data/fleet_service_type_data.xml',
-        'views/account_move_views.xml',
-        'views/fleet_vehicle_views.xml',
+    "name": "Accounting/Fleet bridge",
+    "category": "Accounting/Accounting",
+    "summary": "Manage accounting with fleets",
+    "description": "",
+    "version": "15.0.1.0.0",
+    "depends": [
+        "fleet",
+        "account"
     ],
-    'installable': True,
-    'auto_install': True,
-    'application': False,
-    'license': 'LGPL-3',
+    "data": [
+        "data/fleet_service_type_data.xml",
+        "views/account_move_views.xml",
+        "views/fleet_vehicle_views.xml"
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+    "license": "LGPL-3"
 }
